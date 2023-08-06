9809ac109e0da5c21b8e95173b65629c7d521786
vsumit89 <sumit.vishwakarma@factly.in>
2023-08-06 21:14:44 +0530

    updated the function name

--- app/worker.py
+++ app/worker.py
@@ -20,9 +20,15 @@ def generate_report(report_id: uuid.UUID):
     generate_report is a celery task which is used to generate the report
     it is called by the create_report endpoint
     """
-    async def update():
+    async def update_report_url_and_status():
+        """
+        update_report_url_and_status is an async function which is used to update the report url and status
+
+
+        """
         async with get_session() as session:
             repo = ReportRepository(session)
+            # time.sleep(20)
             response = await repo.update_report_url(report_id, 'https://www.google.com')
             if response.status != "COMPLETED":
                 raise Exception("Unable to update report url")
@@ -30,7 +36,7 @@ def generate_report(report_id: uuid.UUID):
                 print("Report url updated successfully")
 
     try:
-        asyncio.run(update())
+        asyncio.run(update_report_url_and_status())
     except Exception as e:
         print(str(e))
         raise Exception("Unable to update report url")
